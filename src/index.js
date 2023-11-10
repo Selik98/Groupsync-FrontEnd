b2049e7e41de11fc1c0b8660ba323893a4cfd999
Matias Kiesel <mkiesel98@gmail.com>
2023-11-10 00:03:18 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from './pages/home/Home';
+import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>,
+    },
+  ]);
+
+root.render(
+    <React.StrictMode> 
+        <RouterProvider router={router}/>
+    </React.StrictMode>
+);
+
+    
+reportWebVitals();
---
+++ src/pages/home/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <>
+      <h2>Con Musul Man</h2>
+    </>
+  );
+}
+
+export default Home;
